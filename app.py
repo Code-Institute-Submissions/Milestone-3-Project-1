76a6175ff206bc6271ea9b42e1912a98c5408cee
David Caffrey <dave.caffrey71@gmail.com>
2020-02-19 15:23:04 +0000

    create basic templates

--- app.py
+++ app.py
@@ -1,14 +1,25 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
-def hello():
-    return "Hello Dave...."
+def index():
+    return render_template("index.html")
+
+
+@app.route('/signup')
+def signup():
+    return render_template("signup.html")
+
+
+@app.route('/login')
+def login():
+    return render_template("login.html")
 
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
-        port=int(os.environ.get("PORT")),
-        debug=True)
+            port=int(os.environ.get("PORT")),
+            debug=True)
