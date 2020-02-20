fc67bebc4a0b79410b0734f88637e868100c1068
David Caffrey <dave.caffrey71@gmail.com>
2020-02-20 09:24:19 +0000

    play with layout

--- app.py
+++ app.py
@@ -6,17 +6,22 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("index.html", page_title="About")
 
 
 @app.route('/signup')
 def signup():
-    return render_template("signup.html")
+    return render_template("signup.html", page_title="Sign Up")
 
 
 @app.route('/login')
 def login():
-    return render_template("login.html")
+    return render_template("login.html", page_title="Login")
+
+
+@app.route('/workspace')
+def workspace():
+    return render_template("workspace.html", page_title="Workspace", user= "DAVE CAFFREY")
 
 
 if __name__ == "__main__":
