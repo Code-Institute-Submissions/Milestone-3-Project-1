d6569b8903043b81761f4ab06355e8d6a37cac03
David Caffrey <dave.caffrey71@gmail.com>
2020-02-23 17:26:09 +0000

    add cards and images

--- app.py
+++ app.py
@@ -1,20 +1,21 @@
 import os
 import json
+# from the flask module use Flask class and render template function
 from flask import Flask, render_template
-
+#instantiate Flask class and ref with app 
 app = Flask(__name__)
 
-
+# first template to index/home page
 @app.route('/')
 def index():
-    return render_template("index.html", page_title="About")
+    return render_template("index.html", page_title="Home")
 
-
+# template for signup page
 @app.route('/signup')
 def signup():
     return render_template("signup.html", page_title="Sign Up")
 
-
+# template for login page 
 @app.route('/login')
 def login():
     return render_template("login.html", page_title="Login")
@@ -24,8 +25,9 @@ def login():
 def workspace():
     return render_template("workspace.html", page_title="Workspace", user="DAVE CAFFREY")
 
-
+# call to Flask Class run function passing in 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
+            #remove debug = True from production code
             debug=True)
