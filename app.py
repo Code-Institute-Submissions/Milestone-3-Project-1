0c87e637f38b058f779698d0d5b9842905a21305
David Caffrey <dave.caffrey71@gmail.com>
2020-02-27 10:00:29 +0000

    create two connections to DB and start to implement wtforms

--- app.py
+++ app.py
@@ -3,7 +3,9 @@ import os
 import json
 from datetime import datetime
 # from the flask module use Flask class and render template function
-from flask import Flask, render_template, request, flash, session
+from flask import Flask, render_template, request, flash, session, redirect, url_for, logging
+from wtforms import Form, BooleanField, StringField, TextAreaField, PasswordField, validators
+from passlib.hash import sha256_crypt
 # instantiate Flask class and ref with ap
 from os import path
 if path.exists("env.py"):
@@ -30,8 +32,10 @@ def mongo_connect(url):
 date_time = datetime.now().strftime("%Y:%M:%D:%H:%M:%S")
 
 
-conn = mongo_connect(MONGODB_URI)
-coll = conn[DBS_NAME][COLLECTION_NAME]
+conn_blog = mongo_connect(MONGODB_URI)
+conn_users = mongo_connect(MONGODB_URI)
+coll_blog = conn_blog[DBS_NAME][COLLECTION_NAME]
+coll_users = conn_users[DBS_NAME][COLLECTION_NAME]
 
 
 # first template to index/home page
@@ -42,10 +46,15 @@ def index():
 # template for signup page
 @app.route('/signup', methods=["GET", "POST"])
 def signup():
-    if request.method == "POST":
+    form = RegistrationForm(request.form)
+    if request.method == "POST" and form.validate():
+        user = User(form.name.data, form.username.data, form.email.data,
+                    form.password.data)
+        coll_users.insert(user)
         flash("Thanks {}, we have recieved your message".format(
             request.form["fname"]))
-    return render_template("signup.html", page_title="Sign Up")
+        return redirect('login')
+    return render_template("signup.html", page_title="Sign Up" )
 
 # template for login page
 @app.route('/login', methods=["GET", "POST"])
@@ -62,7 +71,7 @@ def login():
 def workspace():
     data = []
     try:
-        data = coll.find()
+        data = coll_blog.find()
         print(data)
     except:
         print("Errrorrrrrr")
@@ -78,7 +87,7 @@ def workspace():
 @app.route('/workspace/<blog_title>/', methods=["GET", "POST"])
 def workspace_blog(blog_title):
     blog = {}
-    blog = coll.find_one({'title': blog_title})
+    blog = coll_blog.find_one({'title': blog_title})
      
     return render_template("blog.html", blog=blog)
 
@@ -89,3 +98,15 @@ if __name__ == "__main__":
             port=int(os.environ.get("PORT")),
             # remove debug = True from production code
             debug=True)
+
+
+class RegistrationForm(Form):
+    name = StringField('Name', [validators.Length(min=5, max=50)])
+    username = StringField('Username', [validators.Length(min=4, max=25)])
+    email = StringField('Email Address', [validators.Length(min=6, max=35)])
+    password = PasswordField('Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='The Passwords must match to proceed')
+    ])
+    confirm = PasswordField('Repeat Password')
+    accept_tos = BooleanField('I accept the TOS', [validators.DataRequired()])
